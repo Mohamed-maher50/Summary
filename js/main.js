d84f8ab2578f7db0f2aad0d52b4d6e271fb5d8cb
Mohamed-maher50 <mm3517608@gmail.com>
2021-10-05 12:49:50 +0200

    modify nice scroll

--- js/main.js
+++ js/main.js
@@ -5,15 +5,13 @@ document.querySelectorAll("body")[0].onscroll = function () {
 };
 $(document).ready(function () {
   $("body").niceScroll({
-    scrollspeed: "0",
     zindex: 6,
     cursorwidth: "10",
 
     cursorcolor: "#e2a355",
     mousescrollstep: 50,
     smoothscroll: true,
-    hwacceleration: true,
-    boxzoom: false,
+
     smoothscroll: true,
   });
 });
